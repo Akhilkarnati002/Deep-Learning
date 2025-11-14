bda50890262a953b4abb44c099e9c8bf8250e7fe
Ahmed Zaheer Hussain <syedahmedzaheerahz@gmail.com>
2025-11-14 11:13:23 +0100

    created Transformer.py file which will be converting raw images into tensor that can be fed to NN

---
+++ Utils/transformers.py
@@ -0,0 +1,33 @@
+from torchvision import transforms
+
+"""
+This module provides a set of common image transformation pipelines
+for training and testing machine learning models using the torchvision library.
+
+Image Processing / Augmentation Pipelines for IR Images
+Input PIL Images 
+Output torch Tensors [-1,1]    
+"""
+
+
+
+IMAGE_SIZE = 256        # default, can be overridden by config
+
+# Basic Transformation Pipeline
+transform_pipeline = transforms.Compose ([ 
+    transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(10),      # 10 Degrees Rotation 
+    transforms.ToTensor(),
+    transforms.Normalize([0.5,], [0.5,])    # normalize to [-1,1]
+    
+    ])
+
+# Optional Augmentation Pipeline
+augment_pipleine = transforms.Compose ([ 
+    transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(10),      # 10 Degrees Rotation
+    transforms.ToTensor(),
+    transforms.Normalize([0.5,], [0.5,])    # normalize to [-1,1]
+    ])
